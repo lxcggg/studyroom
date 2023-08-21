6c2643326a86e96a3a8a14f512db1fbf79aceea7
Kajiura Yuki <3061740475@qq.com>
2023-08-21 16:50:15 +0800

    xyh test

--- src/main/java/com/room/studyroom/StudyroomApplication.java
+++ src/main/java/com/room/studyroom/StudyroomApplication.java
@@ -9,7 +9,7 @@ public class StudyroomApplication {
         SpringApplication.run(StudyroomApplication.class, args);
         System.out.println("hello world");
         System.out.println("xyh test");
-        System.out.println("xyh test3dsadas");
+        System.out.println("xyh test");
     }
 
 }
