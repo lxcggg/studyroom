ddc4f3a368a21449e143f471baf21bcba0f5d4df
lxc14 <1447234802@qq.com>
2023-08-22 09:23:04 +0800

    111

--- src/main/java/com/room/studyroom/StudyroomApplication.java
+++ src/main/java/com/room/studyroom/StudyroomApplication.java
@@ -1,14 +1,24 @@
 package com.room.studyroom;
 
 
+import org.mybatis.spring.boot.autoconfigure.MybatisAutoConfiguration;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
-public class StudyroomApplication {
-
+@SpringBootApplication(exclude = { DataSourceAutoConfiguration.class // 关闭mybatis的自动配置
+        , MybatisAutoConfiguration.class })
+@EnableScheduling
+public class StudyroomApplication extends SpringBootServletInitializer {
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(StudyroomApplication.class);
+    }
     public static void main(String[] args) {
         SpringApplication.run(StudyroomApplication.class, args);
-        System.out.println("hello world");
-        System.out.println("lxc test");
     }
-
 }
+
